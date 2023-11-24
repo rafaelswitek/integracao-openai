b9440d58486961a514daf03dda8a541650d5ac57
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-11-24 00:14:00 -0300

    01. Integração com API

---
+++ integracao_api.py
@@ -0,0 +1,21 @@
+import openai
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+resposta = openai.ChatCompletion.create(
+    model = "gpt-3.5-turbo",
+    messages = [
+        {
+            "role": "system",
+            "content": "Gere nomes de produtos fictícios sem descrição de acordo com a requisição do usuário."
+        },
+        {
+            "role": "user",
+            "content": "Gere 5 produtos"
+        } 
+    ]
+)
+print(resposta)
