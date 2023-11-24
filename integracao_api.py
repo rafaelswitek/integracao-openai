31a782b31a6f4334f3284d4610d4ba7ee3d6fcae
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-11-24 00:38:00 -0300

    02. Prompt engineering e prompt template

--- integracao_api.py
+++ integracao_api.py
@@ -1,21 +1,46 @@
+import os
 import openai
 import dotenv
-import os
+
+def categorizaProduto(nome_do_produto, categorias_validas):
+    prompt_sistema = f"""
+    Você é um categorizador de produtos.
+    Você deve escolher uma categoria da lista abaixo:
+    Se as categorias informadas não forem categorias validas, responda com "Não posso ajudá-lo com isso"
+    ##### Lista de categorias válidas
+    {categorias_validas}
+    ##### Exemplo
+    bola de tênis
+    Esportes
+    """
+    resposta = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "system",
+        "content": prompt_sistema
+        },
+        {
+            "role": "user",
+            "content": nome_do_produto
+        }
+    ],
+    temperature=1,
+    max_tokens=256,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+    )
+    print(resposta.choices[0].message.content)
+
 
 dotenv.load_dotenv()
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
-resposta = openai.ChatCompletion.create(
-    model = "gpt-3.5-turbo",
-    messages = [
-        {
-            "role": "system",
-            "content": "Gere nomes de produtos fictícios sem descrição de acordo com a requisição do usuário."
-        },
-        {
-            "role": "user",
-            "content": "Gere 5 produtos"
-        } 
-    ]
-)
-print(resposta)
+
+print("Digite as categorias validas:")
+categorias_validas = input()
+while True:
+    print("Digite o nome do produto:")
+    nome_do_produto = input()
+    categorizaProduto(nome_do_produto, categorias_validas)
